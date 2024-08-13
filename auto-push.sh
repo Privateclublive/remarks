40e9a267df5cdf3f659128a29cf540a6541167d8
Privateclublive <alecfasani@mac.com>
2024-08-13 18:46:44 +0200

    Update workflow and script for debugging

--- auto-push.sh
+++ auto-push.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+set -e  # Exit immediately if a command exits with a non-zero status
+set -x  # Print commands and their arguments as they are executed
+
 cd ~/Remarks
+git status
 git add .
-git commit -m "Automated commit"
+git commit -m "Automated commit" || echo "Nothing to commit"
 git push origin main
